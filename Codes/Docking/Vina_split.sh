9b06b997fe510230b4ac38b6dee75de16b818262
Raghu102433 <raghava.332410@gmail.com>
2025-03-31 00:20:31 +0530

    docking

---
+++ Codes/Docking/Vina_split.sh
@@ -0,0 +1,7 @@
+#Autoock vina docking 
+#Vina_split execution command to get conformers and analyse interactions
+
+C/Users/ragha/.mgltools/Pyrx/6ny7> vina_split --input ligand_out.pdbqt
+
+#make sure ligand and vina_split are in same folder for smooth execution
+ 
